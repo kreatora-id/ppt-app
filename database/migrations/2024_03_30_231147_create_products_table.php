13550abfdf6e0ed8f000da7be794fd9b3af32f43
nuwasdzarrin3 <nuwasdzarrin3@gmail.com>
2024-03-31 06:27:25 +0700

    add product

---
+++ database/migrations/2024_03_30_231147_create_products_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->text('faq')->nullable();
+            $table->string('slug');
+            $table->string('embed')->nullable();
+            $table->float('regular_price')->default(0);
+            $table->float('price')->default(0);
+            $table->json('images')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
