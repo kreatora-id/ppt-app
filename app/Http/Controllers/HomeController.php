22d98043eb00f462cdd17951867a2ac843ae6e06
nuwasdzarrin3 <nuwasdzarrin3@gmail.com>
2024-04-02 08:30:31 +0700

    add tag options

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use TCG\Voyager\Models\DataRow;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        $tag_options = DataRow::query()->where('field', 'tags')->first();
+        $tag_options = $tag_options && $tag_options->details && $tag_options->details->options ? $tag_options->details->options : [];
+        dd($tag_options->toArray());
         return view('home');
     }
 
